57cafdc64bad8d193869af5e3d63516b42fbf045
virkumar <virkumar@microsoft.com>
2022-01-16 18:58:09 +0530

    first commmit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/MercSpringJavaAdvance-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,25 @@
+package com.example;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@SpringBootApplication
+public class Main {
+
+  @Value("${spring.datasource.url}")
+  private String dbUrl;
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(Main.class, args);
+  }
+
+  @RequestMapping("/")
+  String index() {
+    return "index";
+  }
+}
